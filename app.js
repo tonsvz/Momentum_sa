5580779fcd0136e3a341bc815c4c1ff863c83f39
Tony <tonsvz@gmail.com>
2021-05-01 22:18:37 -0500

    hipervinculos agregados

--- app.js
+++ app.js
@@ -12,57 +12,64 @@
 //   ease: Circ.Out,
 // });
 
-TweenMax.to(".left", 2, {
+TweenMax.to(".left", 1, {
   delay: 0.8,
   width: "50%",
   ease: Power2.easeInOut,
 });
-TweenMax.to(".right", 2, {
+
+TweenMax.to(".right", 1, {
   delay: 0.6,
   width: "50%",
   ease: Power3.easeInOut,
 });
-TweenMax.from("nav", 2, {
+
+TweenMax.from("nav", 1, {
   delay: 0.6,
   opacity: 0,
   ease: Expo.easeInOut,
 });
+
 TweenMax.from(".text h1", 2, {
   delay: 0.6,
-  x: 1000,
-  ease: Circ.easeInOut,
-});
-
-TweenMax.from(".text p", 2, {
-  delay: 0.7,
-  x: 1000,
-  ease: Circ.easeInOut,
-});
-
-TweenMax.to(".momentum", 2, {
-  delay: 1.5,
-  width: "1000px",
+  x: 90,
+  opacity: -0.1,
   ease: Power2.easeInOut,
 });
+
+TweenMax.from(".text p", 1.5, {
+  delay: 1,
+  opacity: 0,
+  ease: Power3.easeInOut,
+});
+
 TweenMax.staggerFrom(
   ".bottomNav ul li",
-  2,
+  1,
   {
     delay: 1,
-    x: 1000,
+    x: 100,
+    opacity: -1,
     ease: Circ.easeInOut,
   },
   0.08
 );
 
-TweenMax.from(".info", 2, {
+TweenMax.from(".info", 1, {
   delay: 1.5,
-  y: 100,
+  y: 0,
+  opacity: 0,
   ease: Circ.easeInOut,
 });
 
-TweenMax.from(".name", 2, {
+TweenMax.from(".name", 1, {
   delay: 1.5,
-  x: -600,
+  x: -500,
+  opacity: -1.5,
   ease: Circ.easeInOut,
+});
+TweenMax.to(".momentum", 1, {
+  delay: 1.5,
+  width: "1000px",
+  ease: Power2.easeInOut,
 });
